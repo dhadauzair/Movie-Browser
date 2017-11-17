4d0198621ca0dbd77fa4b002961fcb4ce168a11d
Uzair Dhada <dhadauzair@gmail.com>
2017-11-18 03:20:05 +0530

    Initial commit

---
+++ Movie Browser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Movie Browser
+//
+//  Created by Uzair Dhada on 18/11/17.
+//  Copyright © 2017 Uzair Dhada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Movie Browser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Movie Browser
+//
+//  Created by Uzair Dhada on 18/11/17.
+//  Copyright © 2017 Uzair Dhada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
